38e21f5ec31a073825629cda1006ce96838c03eb
Flaper Fesp <flaper87@gmail.com>
2013-06-11 09:27:34 +0200

    Changed header
    Change-Id: I2b6370aefa5b0408efc2b510e3acb22dc17072f1

--- openstack/common/cache/cache.py
+++ openstack/common/cache/cache.py
@@ -12,7 +12,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-"""Multiple Cache API backend support.
+"""Cache library.
 
 Supported configuration options:
 
